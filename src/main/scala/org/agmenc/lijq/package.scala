33f072bf6a94dd862588e170e05f6e11b97f8d88
agmenc <chris@Christopher-Agmen-Smiths-MacBook-Pro.local>
2012-12-07 18:10:49 +0000

    Fix issue 2 - Ban the dreaded JsCmds._ implicit import.

--- src/main/scala/org/agmenc/lijq/package.scala
+++ src/main/scala/org/agmenc/lijq/package.scala
@@ -1,9 +1,11 @@
 package org.agmenc
 
 import net.liftweb.http.js.JsExp
+import net.liftweb.http.js.JsCmds._
 
 package object lijq {
   implicit def jsExpToJQuery(jsExp: JsExp): JQuery = new JQuery(jsExp)
+  implicit def jsExpToJsCmd2(jsExp: JsExp) = jsExpToJsCmd(jsExp)
 
   val $ = Dollar
 }
