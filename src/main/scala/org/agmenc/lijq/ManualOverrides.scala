2a8bfafa780c68e124172d2f402a54090e093f63
agmenc <chris@Christopher-Agmen-Smiths-MacBook-Pro.local>
2012-12-13 18:17:36 +0000

    Add some more NodeSeq-friendly methods

--- src/main/scala/org/agmenc/lijq/ManualOverrides.scala
+++ src/main/scala/org/agmenc/lijq/ManualOverrides.scala
@@ -1,11 +1,19 @@
 package org.agmenc.lijq
 
 import xml.NodeSeq
-import net.liftweb.http.js.jquery.JqJE.JqAppend
+import net.liftweb.http.js._
+import jquery.JqJE.JqAppend
+import jquery.JqJE.JqPrepend
 
 trait ManualOverrides { self: JQueryLike =>
   def append(ns: NodeSeq) = underlying ~> JqAppend(ns)
+  def prepend(ns: NodeSeq) = underlying ~> JqPrepend(ns)
+  def before(ns: NodeSeq) = underlying ~> make("before", ns)
+  def after(ns: NodeSeq) = underlying ~> make("after", ns)
 
+  case class make(functionName: String, content: NodeSeq) extends JsExp with JsMember {
+    override val toJsCmd = functionName + "(" + fixHtmlFunc("inline", content){str => str }+ ")"
+  }
 
   /*
   Legend:
